ec87d47f246ed6e0cea472860074a2f140a46a26
Kaylob Kennedy <Kaylob Kennedy>
2016-11-05 06:00:02 -0500

    no message

--- src/main/java/com/game/enums/Stat.java
+++
@@ -1,6 +0,0 @@
-package com.game.enums;
-
-public enum Stat {
-	HP,
-	DAMAGE,FIRERATE,NUM_PROJECTILE,BULLET_SPEED;
-}
--- src/main/java/com/game/enums/State.java
+++
@@ -1,9 +0,0 @@
-package com.game.enums;
-
-public enum State {
-	Main_Menu,
-	Options,
-	Level_Select,
-	Game,
-	Pause, None, Start_Menu, Character_Menu, Loading, Options_Controls, FullscreenNotify;
-}
